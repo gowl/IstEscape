e1519db634bc5337ae9bc8d6020f89b0f856d7ff
Ghaith <Ghaith.m.alhasan@gmail.com>
2023-05-16 22:37:32 +0300

    build the seeder to generate users, escape rooms, and bookings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Booking;
+use App\Models\EscapeRoom;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +19,47 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //remove the tables before seeding them again
+        Schema::disableForeignKeyConstraints();
+        User::truncate();
+        EscapeRoom::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        //generate records
+        User::factory(50)->create();
+
+        EscapeRoom::create([
+            'theme' => 'The Philosopher\'s Stone',
+            'max_participants' => 5,
+        ]);
+
+        EscapeRoom::create([
+            'theme' => 'Horror Mansion',
+            'max_participants' => 8,
+        ]);
+
+        EscapeRoom::create([
+            'theme' => 'Back to The Past',
+            'max_participants' => 3,
+        ]);
+
+        for ($userId = 1; $userId <= User::all()->count(); $userId++) {
+            $this->createBooking($userId);
+        }
+    }
+
+    private function createBooking($userId)
+    {
+        //random date a week ahead (max) and within opening hours (11am-9pm)
+        $beginsAt = Carbon::today()->hour(rand(11, 20))->addDays(rand(0, 7));
+        $endsAt = Carbon::parse($beginsAt)->addHour();
+        //Note: This mock data does not account for the max participants being exceeded in a specific time slot of an escape room
+
+        Booking::create([
+            'escape_room_id' => rand(1, 3),
+            'user_id' => $userId,
+            'begins_at' => $beginsAt,
+            'ends_at' => $endsAt,
+        ]);
     }
 }
